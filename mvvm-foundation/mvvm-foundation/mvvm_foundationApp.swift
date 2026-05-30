5eb39963b4666a2adf8f28ba0e2f108967b87785
Panagiotis Palamidas <75530751+Mesrine23@users.noreply.github.com>
2026-05-30 18:37:39 +0300

    Project init

---
+++ mvvm-foundation/mvvm-foundation/mvvm_foundationApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvm_foundationApp.swift
+//  mvvm-foundation
+//
+//  Created by Mesrine on 30/5/26.
+//
+
+import SwiftUI
+
+@main
+struct mvvm_foundationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
